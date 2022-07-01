356c9fd9db7261bfb64b854f8ef0b4ae9d671b5e
Ramabotz9 <108044415+Ramabotz9@users.noreply.github.com>
2022-07-01 13:43:46 +0800

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/abff2f2c3398c30703d47.png', m, { packname: "Stiker", author: "By ZxyuuBotz" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/abff2f2c3398c30703d47.png', m, { packname: "Stiker", author: "By Rama" })
 }
 
 handler.customPrefix = /^(cok|anj|anjg|anjing|gblk|goblok|goblog|asu|ashu|asw|ngntd|ngentod|memek|mmk|kontol|kntl)$/i
